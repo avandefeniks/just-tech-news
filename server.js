cb2d4752f995e964b536c30693ed5113d6a57e89
avandefeniks <angel.vandefeniks@gmail.com>
2022-06-06 19:07:11 -0700

    working on capturing login credentials

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
 
-// turn on connection to db server
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+// turn on connection to db and server
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
