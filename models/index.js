89e16860e0f29bd2c8c7908d35dfa5ad93f83f2b
avandefeniks <angel.vandefeniks@gmail.com>
2022-06-08 15:47:26 -0700

    post routes completed

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
+const { userInfo } = require('os');
+
+// create association
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
 module.exports = { User, Post };
